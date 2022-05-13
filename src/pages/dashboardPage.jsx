e1957bdf2b4f38066e6be49f21e50245be32fc31
Alexander Ilchenko <ilchenko.alexander.n@gmail.com>
2022-05-13 11:30:33 +0300

    add HomeTab (Table styles + temp hardcode markup)

---
+++ src/pages/dashboardPage.jsx
@@ -0,0 +1,13 @@
+import HomeTab from "../components/HomeTab/HomeTab";
+
+export default function DashboardPage() {
+    
+    return (
+      <>
+       {/*Header???*/}
+        <HomeTab />
+        {/*Curency????*/}
+        {/*Diagram??*/}
+      </>
+    )
+  }
