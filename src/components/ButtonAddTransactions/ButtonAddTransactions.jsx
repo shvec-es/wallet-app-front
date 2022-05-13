17021fa87f441bd462a37644266db8cf040e310f
shvec-es <kotova.es.1510@gmail.com>
2022-05-13 11:50:18 +0300

    add button

---
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -0,0 +1,5 @@
+const ButtonAddTransactions = () => {
+    return <button><svg><use></use></svg></button>
+}
+
+export default ButtonAddTransactions;
---
+++ src/components/ButtonAddTransactions/index.js
@@ -0,0 +1,3 @@
+import ButtonAddTransactions from './ButtonAddTransactions';
+
+export default ButtonAddTransactions;
