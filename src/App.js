9b5144f2822af016bd6df693461115c72e86e159
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-05-15 11:29:24 +0200

    add background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { Suspense } from 'react';
-import { Routes, Route } from "react-router-dom";
-import LoginPage from "components/LoginPage/LoginPage";
-import Container from 'components/Container';
+import { Suspense, lazy } from 'react';
+import Media from 'react-media';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from 'components/LoginPage/LoginPage';
+import { BackGround, Container } from 'components';
 import DashboardPage from 'pages/dashboardPage';
 
 function App() {
   return (
-    <Container>
-      <h1>Wallet</h1>
-      <Suspense fallback={
-        <p>Loading...</p>
-      }>
-        <Routes>
-          <Route path='/' element={ <LoginPage/>}>
-          </Route>
-        </Routes>
-        <DashboardPage />      
-      </Suspense>
-    </Container>
+    <>
+      <Media query="(min-width: 768px)">
+        <BackGround />
+      </Media>
+
+      <Container>
+        <h1>Wallet</h1>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route path="/" element={<LoginPage />}></Route>
+          </Routes>
+          <DashboardPage />
+        </Suspense>
+      </Container>
+    </>
   );
 }
 
