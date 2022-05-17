d0ce91e8d7ba70287f54e3b53d3731390be257d4
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-05-17 22:29:11 +0200

    add Logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Suspense, lazy } from 'react';
+import { Suspense } from 'react';
 import Media from 'react-media';
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from 'components/LoginPage/LoginPage';
-import { BackGround, Container } from 'components';
 import DashboardPage from 'pages/dashboardPage';
-import HeaderPage from 'pages/headerPage';
-import Navigation from 'components/Navigation';
-import Balance from 'components/Balance';
-import Header from './components/Header'
+import {
+  BackGround,
+  Container,
+  Header,
+  Navigation,
+  LoginPage,
+} from 'components';
 
 function App() {
   return (
@@ -17,9 +18,7 @@ function App() {
       </Media>
       <Header />
       <Container>
-        {/* <HeaderPage /> */}
         <Navigation />
-        <Balance />
         <Suspense fallback={<p>Loading...</p>}>
           <Routes>
             <Route path="/" element={<LoginPage />}></Route>
--- src/services/index.js
+++
@@ -1,4 +0,0 @@
-import i18next from './i18n';
-import getCurrency from './prvat-bank-api';
-
-export { i18next, getCurrency };
