2ddfa3ec406bfc443f23ad2946831705d6afd1e3
Dima Sotskiy <sotskiydr@gmail.com>
2022-05-19 00:11:24 +0300

    add Api Services

---
+++ src/services/ApiServices.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://wallet-codewriters.herokuapp.com';
+
+class ApiServices {
+  async signIn(userData){
+    try {
+      const { data } = await axios.post('/api/auth/login', userData)
+      console.log(data.payload)
+      return data.payload
+    }catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  async logout(token){
+    try {
+      const req = await axios.get('/api/auth/logout', { headers: { Authorization: `Bearer ${token}` } })
+      return req.data
+    }catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  async signUp(userData){
+    try {
+      const { data } = await axios.post('/api/auth/signup', userData)
+      console.log(data.payload)
+      return data.payload.user
+    }catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  async getCategories(token){
+    try {
+      const req = await axios.get('/wallet/transaction/categories', { headers: { Authorization: `Bearer ${token}` } })
+      return req.data.payload
+    }catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  async getTransactions(token){
+    try {
+      const req = await axios.get('/wallet/transactions', { headers: { Authorization: `Bearer ${token}` } })
+      return req.data.payload.transactions
+    }catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  async getStats({ month, year },token){
+    try {
+      const req = await axios.get(`wallet/stats?month=${month}&year=${year}`,
+        { headers: { Authorization: `Bearer ${token}` } })
+      return req.data.payload
+    }catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  async createTransaction(userData, token){
+    try {
+      const { data } = await axios.post('/wallet/transaction', userData,
+        { headers: { Authorization: `Bearer ${token}` }})
+      return data
+    }catch (e) {
+      console.log(e.message)
+    }
+  }
+
+}
+
+export default new ApiServices
+
