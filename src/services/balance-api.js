903c1146400efba234f709416752d12cdcfd75d2
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-05-19 12:25:48 +0200

    balance-api fix

--- src/services/balance-api.js
+++ src/services/balance-api.js
@@ -2,18 +2,13 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://wallet-codewriters.herokuapp.com';
 
-const d = new Date();
-const currMonth = '0' + (d.getMonth() + 1);
-const currYear = d.getFullYear();
+const date = new Date();
+const currMonth = '0' + (date.getMonth() + 1);
+const currYear = date.getFullYear();
 
-let JWTToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyODNiNjM1NjdiYjk0YTVhOGY5Y2QzMyIsImlhdCI6MTY1Mjg5MTM2NSwiZXhwIjoxNjUyODk0OTY1fQ.fDUDjLnqle09tNnooH78UhKvsya6ioBLXmrzL_rBM58';
 async function getBalance() {
   const { data } = await axios.get(
     `/wallet/stats?year=${currYear}&month=${currMonth}`,
-    {
-      headers: { Authorization: `Bearer ${JWTToken}` },
-    },
   );
   return data.payload.balance.balance;
 }
