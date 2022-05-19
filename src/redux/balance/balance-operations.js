88d9fdc37a0568e04abf8c2e943f16387cbbc521
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-05-19 12:28:06 +0200

    balance-operations edit

--- src/redux/balance/balance-operations.js
+++ src/redux/balance/balance-operations.js
@@ -7,14 +7,10 @@ import {
 
 export const fetchBalance = async dispatch => {
   dispatch(fetchBalancePending);
-  console.log('in balance-operations fetchBalancePending');
   const balance = await getBalance();
-
   try {
     dispatch(fetchBalanceSuccess(balance));
-    console.log('in balance-operations111 success');
   } catch (error) {
-    console.log('in balance-operations222 error');
     dispatch(fetchBalanceError);
   }
 };
