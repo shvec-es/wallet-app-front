8df6b5232c7b806caccddf9215b4058f8ff69b9f
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-05-21 13:20:33 +0200

    add table translate

--- src/components/HomeTab/Table.styled.js
+++ src/components/HomeTab/Table.styled.js
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
 
 export const TableMain = styled.table`
-    width: 715px;
-    max-height: 60vh;
-    @media screen and (min-width: 1280px) {
-        width: 745px;
-      }
-`
+  width: 715px;
+  max-height: 60vh;
+  @media screen and (min-width: 1280px) {
+    width: 745px;
+  }
+`;
 export const TableHeader = styled.thead`
-    display: block;
-    padding: 7px 20px;
-    font-size: 18px;
-    font-weight: 700;
-    background-color: var(--color-white);
-    border-radius: 30px;
-`
+  display: block;
+  padding: 7px 20px;
+  font-size: 18px;
+  font-weight: 700;
+  background-color: var(--color-white);
+  border-radius: 30px;
+`;
 export const TableBody = styled.tbody`
-    display: block;
-    padding: 0px 20px 15px 20px;
-    font-size: 16px;
-`
+  display: block;
+  padding: 0px 20px 15px 20px;
+  font-size: 16px;
+`;
 
 export const ButtonDelete = styled.button`
   background-color: transparent;
@@ -35,30 +35,32 @@ export const ButtonDelete = styled.button`
   &:active {
     transform: scale(0.7);
   }
-`
+`;
 export const TableRow = styled.tr`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px 0;
-    transition: transform 150ms var(--cubic-bezier);
-    &:not(:last-child) {
-        border-bottom: 1px solid #dcdcdf;
-        box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
-      }
-      &:hover {
-        transform: scale(1.02) translate( 3px,-1px);
-        font-weight: 600;
-      }
-      &:hover ${ButtonDelete}{
-          height: auto;
-          opacity: 1;
-          visibility: visible;
-      }
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+  transition: transform 150ms var(--cubic-bezier);
+  &:not(:last-child) {
+    border-bottom: 1px solid #dcdcdf;
+    box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
+  }
+  &:hover {
+    transform: scale(1.02) translate(3px, -1px);
+    font-weight: 600;
+  }
+  &:hover ${ButtonDelete} {
+    height: auto;
+    opacity: 1;
+    visibility: visible;
+  }
+`;
 
 export const TableHeadCell = styled.th`
-  font-weight: 700;`
+  font-weight: 700;
+  text-transform: capitalize;
+`;
 
 export const TableCell = styled.td`
   text-align: center;
@@ -67,24 +69,27 @@ export const TableCell = styled.td`
     width: 70px;
     text-align: start;
   }
-&:nth-child(2) {
-  width: 40px;
-  margin-right: 20px; 
+  &:nth-child(2) {
+    width: 40px;
+    margin-right: 20px;
   }
-&:nth-child(3) {
+  &:nth-child(3) {
     width: 120px;
     text-align: start;
   }
-&:nth-child(4) {
+  &:nth-child(4) {
     width: 120px;
     text-align: start;
   }
-&:last-child {
-    
+  &:last-child {
     text-align: end;
   }
-`
+`;
 export const TableCellColor = styled.td`
   width: 80px;
-  text-align: end;  
-color: ${props => (props.type === 'income' ? 'var(--color-secondary)' : 'var(--color-tertiary)')};`
+  text-align: end;
+  color: ${props =>
+    props.type === 'income'
+      ? 'var(--color-secondary)'
+      : 'var(--color-tertiary)'};
+`;
--- src/components/HomeTab/TableMobile.jsx
+++ src/components/HomeTab/TableMobile.jsx
@@ -1,39 +1,65 @@
-import { MobileCard, MobileCardGroup, MobileCardItem, MobileCardItemValue, MobileCardItemColor } from './TableMobile.styled';
+import {
+  MobileCard,
+  MobileCardGroup,
+  MobileCardItem,
+  MobileCardItemValue,
+  MobileCardItemColor,
+} from './TableMobile.styled';
+import { useTranslation } from 'react-i18next';
 
-function TableMobile({data}) {
+function TableMobile({ data }) {
+  const { t } = useTranslation();
+
   return (
-  <>
-    {data.map(({_id, date, typeTransaction, category, description, sum, balance}) => (
-      <MobileCard key={_id} type={typeTransaction === true ? "income" : "costs"}>
-          <MobileCardGroup>
-            <MobileCardItem>Дата</MobileCardItem>
-            <MobileCardItemValue>{date}</MobileCardItemValue>
-          </MobileCardGroup>
-          <MobileCardGroup>
-            <MobileCardItem>Тип</MobileCardItem>
-            <MobileCardItemValue>{typeTransaction === true ? '+' : '-'}</MobileCardItemValue>
-          </MobileCardGroup>
-          <MobileCardGroup>
-            <MobileCardItem>Категория</MobileCardItem>
-            <MobileCardItemValue>{category}</MobileCardItemValue>
-          </MobileCardGroup>
-          <MobileCardGroup>
-            <MobileCardItem>Комментарий</MobileCardItem>
-            <MobileCardItemValue>{description}</MobileCardItemValue>
-          </MobileCardGroup>
-          <MobileCardGroup>
-            <MobileCardItem>Сумма</MobileCardItem>
-            <MobileCardItemColor  type={typeTransaction === true ? "income" : "costs"}>
-              {sum}
-            </MobileCardItemColor>
-          </MobileCardGroup>
-          <MobileCardGroup>
-            <MobileCardItem>Баланс</MobileCardItem>
-            <MobileCardItemValue>{balance}</MobileCardItemValue>
-          </MobileCardGroup>
-      </MobileCard>
-    ) )}
-  </>
+    <>
+      {data.map(
+        ({
+          _id,
+          date,
+          typeTransaction,
+          category,
+          description,
+          sum,
+          balance,
+        }) => (
+          <MobileCard
+            key={_id}
+            type={typeTransaction === true ? 'income' : 'costs'}
+          >
+            <MobileCardGroup>
+              <MobileCardItem>{t('date')}</MobileCardItem>
+              <MobileCardItemValue>{date}</MobileCardItemValue>
+            </MobileCardGroup>
+            <MobileCardGroup>
+              <MobileCardItem>{t('type')}</MobileCardItem>
+              <MobileCardItemValue>
+                {typeTransaction === true ? '+' : '-'}
+              </MobileCardItemValue>
+            </MobileCardGroup>
+            <MobileCardGroup>
+              <MobileCardItem>{t('category')}</MobileCardItem>
+              <MobileCardItemValue>{category}</MobileCardItemValue>
+            </MobileCardGroup>
+            <MobileCardGroup>
+              <MobileCardItem>{t('description')}</MobileCardItem>
+              <MobileCardItemValue>{description}</MobileCardItemValue>
+            </MobileCardGroup>
+            <MobileCardGroup>
+              <MobileCardItem>{t('sum')}</MobileCardItem>
+              <MobileCardItemColor
+                type={typeTransaction === true ? 'income' : 'costs'}
+              >
+                {sum}
+              </MobileCardItemColor>
+            </MobileCardGroup>
+            <MobileCardGroup>
+              <MobileCardItem>{t('balance')}</MobileCardItem>
+              <MobileCardItemValue>{balance}</MobileCardItemValue>
+            </MobileCardGroup>
+          </MobileCard>
+        ),
+      )}
+    </>
   );
 }
 
