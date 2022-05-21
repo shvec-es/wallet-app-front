2617ba12d1e6a6c8e990c4245d9ef13cd7a5c7ad
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-05-21 13:08:37 +0200

    Update LogoForm.jsx

--- src/components/LogoForm/LogoForm.jsx
+++ src/components/LogoForm/LogoForm.jsx
@@ -8,33 +8,12 @@ const LogoForm = () => {
 
   return (
     <Wrapper>
-      {/* <Media
-        queries={{
-          medium: '(min-width: 768px) and (max-width: 1279px)',
-          large: '(min-width: 1280px)',
-        }}
-      >
-        {matches => (
-          <Fragment>
-            {matches.medium && <img src={Icon} alt="Logo" />}
-            {matches.large && <img src={IconBig} alt="Logo" />}
-          </Fragment>
-        )}
-      </Media> */}
-
       <Media query="(min-width: 768px)">
         <Icon>
           <use href={`${icons}#register`} />
         </Icon>
       </Media>
 
-      {/* <Media query="(min-width: 768px)">
-        <img src={Icon} alt="Logo" />
-      </Media>
-
-      <Media query="(min-width: 1240px)">
-        <img src={IconBig} alt="Logo" />
-      </Media> */}
       <Text>{t('app_title')}</Text>
     </Wrapper>
   );
