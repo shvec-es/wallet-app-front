746d94a8586290fd36a8cb8c18a8ac47b715c720
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-05-22 14:17:19 +0200

    removed balance-api.js

--- src/redux/balance/balance-operations.js
+++ src/redux/balance/balance-operations.js
@@ -1,13 +1,22 @@
-import { getBalance } from '../../services/balance-api';
+import axios from 'axios';
+//import { getBalance } from '../../services/balance-api';
 import {
   fetchBalancePending,
   fetchBalanceSuccess,
   fetchBalanceError,
 } from './balance-actions';
 
+axios.defaults.baseURL = 'https://wallet-codewriters.herokuapp.com';
+const date = new Date();
+const currMonth = '0' + (date.getMonth() + 1);
+const currYear = date.getFullYear();
+
 export const fetchBalance = async dispatch => {
   dispatch(fetchBalancePending);
-  const balance = await getBalance();
+  const { data } = await axios.get(
+    `/wallet/stats?year=${currYear}&month=${currMonth}`,
+  );
+  const balance = data.payload.balance.balance;
   try {
     dispatch(fetchBalanceSuccess(balance));
   } catch (error) {
