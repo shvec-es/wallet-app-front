5eac4646ff526a41504be4b3c45ec75d18f82e30
shvec-es <kotova.es.1510@gmail.com>
2022-05-22 17:52:38 +0300

    delete warnnings and add deploy

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,11 +1,9 @@
-import { Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Media from 'react-media';
 import DiagramTab from "components/DiagramTab";
-// import ButtonAddTransactions from "components/ButtonAddTransactions";
-import { Navigation, HomeTab, ButtonAddTransactions, Container} from "components";
+import { Navigation, HomeTab, Container} from "components";
 import { Currency } from "components";
-
-import {MobSidebar, TabWrap, SideBar, MainWrap, HomeTabWrap, CurrencyWrap, TabletBalanceWrap, ButtonWrap} from './DashboardPage.styled'
+import {MobSidebar, TabWrap, SideBar, MainWrap, HomeTabWrap, CurrencyWrap, TabletBalanceWrap} from './DashboardPage.styled'
 import Balance from "components/Balance";
 
 export default function DashboardPage() {
