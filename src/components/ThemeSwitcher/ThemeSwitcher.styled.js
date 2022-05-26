434bf64adcd2e5aa0ca1c255286583196fd1f44b
shvec-es <kotova.es.1510@gmail.com>
2022-05-26 18:31:31 +0300

    fix theme-switcher

--- src/components/ThemeSwitcher/ThemeSwitcher.styled.js
+++ src/components/ThemeSwitcher/ThemeSwitcher.styled.js
@@ -1,17 +1,17 @@
-
 import styled from 'styled-components';
 
 export const Switcher = styled.button`
-    position: absolute;
-    bottom: 20px;
-    left: 20px;
-    padding: 0.5rem;
-    background: none;
-    border: none;
-    font-size: 2rem;
-    cursor: pointer;
-    outline: none;
-    &:hover {
-      color: ${({ theme }) => theme.text};
-    }
-  `;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  padding: 0.5rem;
+  background: none;
+  border: none;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+  z-index: 100;
+  &:hover {
+    color: ${({ theme }) => theme.text};
+  }
+`;
